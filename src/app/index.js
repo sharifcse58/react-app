32faaba382d51af0a8c8f005acbd3f7568dd917f
sharifcse57 <sharifcse57@gmail.com>
2022-02-19 00:10:47 +0600

    some issue fixed

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { Home, About, Blog, Project, Contact, SideBar } from '../components'
