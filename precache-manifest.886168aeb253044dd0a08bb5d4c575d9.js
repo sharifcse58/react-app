f433219c6d5237b01e1982353e4003e4592e6749
sharifcse58 <sharifcse57@gmail.com>
2022-02-18 13:21:03 +0000

    Deploying to gh-pages from @ sharifcse58/react-app@0c993f14a29afe7cc179af0cbb5a3b570accf04c 🚀

--- precache-manifest.886168aeb253044dd0a08bb5d4c575d9.js
+++ precache-manifest.886168aeb253044dd0a08bb5d4c575d9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21766c0ea893d677af3730f98ece971e",
+    "revision": "a102c0fa03e8521ac623c3db69340706",
     "url": "/react-app/index.html"
   },
   {
@@ -8,23 +8,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-app/static/css/2.e92c2c3e.chunk.css"
   },
   {
-    "revision": "60b5d8d853651ac60d68",
-    "url": "/react-app/static/css/main.d6edf0f0.chunk.css"
+    "revision": "c4aef9db6ebc2792a203",
+    "url": "/react-app/static/css/main.46da083a.chunk.css"
   },
   {
     "revision": "1e4b02dcdcb8409f5c4b",
     "url": "/react-app/static/js/2.93ef64f8.chunk.js"
   },
   {
-    "revision": "60b5d8d853651ac60d68",
-    "url": "/react-app/static/js/main.8bb00708.chunk.js"
+    "revision": "c4aef9db6ebc2792a203",
+    "url": "/react-app/static/js/main.7482284f.chunk.js"
   },
   {
     "revision": "9ed022644c6f38b380ac",
     "url": "/react-app/static/js/runtime~main.cbc86c42.js"
   },
   {
-    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
-    "url": "/react-app/static/media/logo.ee7cd8ed.svg"
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
   }
 ]);
