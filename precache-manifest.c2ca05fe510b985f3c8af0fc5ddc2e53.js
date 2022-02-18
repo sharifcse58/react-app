bdc3f37b79ecfe953e557943d2d74263b5a690c4
sharifcse57 <sharifcse57@gmail.com>
2022-02-18 19:54:40 +0600

    Updates

---
+++ precache-manifest.c2ca05fe510b985f3c8af0fc5ddc2e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21766c0ea893d677af3730f98ece971e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "1e4b02dcdcb8409f5c4b",
+    "url": "/react-app/static/css/2.e92c2c3e.chunk.css"
+  },
+  {
+    "revision": "60b5d8d853651ac60d68",
+    "url": "/react-app/static/css/main.d6edf0f0.chunk.css"
+  },
+  {
+    "revision": "1e4b02dcdcb8409f5c4b",
+    "url": "/react-app/static/js/2.93ef64f8.chunk.js"
+  },
+  {
+    "revision": "60b5d8d853651ac60d68",
+    "url": "/react-app/static/js/main.8bb00708.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
