508656c10e77ac8703e52cc3db7338b253ce11bc
sharifcse57 <sharifcse57@gmail.com>
2022-02-19 02:25:45 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.5257076bd8fa9a66e5cc9fa092495e84.js"
+  "/react-app/precache-manifest.d75b7d5b39c9982c04426e3163d655da.js"
 );
 
 self.addEventListener('message', (event) => {
