9c78954ead2ac0f0398ba3ae064f606f3b15923f
sharifcse57 <sharifcse57@gmail.com>
2022-02-19 02:17:59 +0600

    responsive desing update

--- src/app/index.js
+++ src/app/index.js
@@ -1,44 +1,59 @@
-import React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 
-import { Home, About, Blog, Project, Contact, SideBar } from '../components'
-import { MoviesList,MoviesInsert,MoviesUpdate } from '../pages'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  Home,
+  Header,
+  About,
+  Blog,
+  Project,
+  Contact,
+  SideBar,
+} from "../components";
+import { MoviesList, MoviesInsert, MoviesUpdate } from "../pages";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Container = styled.div.attrs({
-    className: 'container',
-})``
+  className: "container",
+})``;
 
 function App(props) {
-    return (
-        <Router>
-            {/* <NavBar /> */}
-            <div class="row">
-                <SideBar />
-                <div class="content">
-                    {/* <Header /> */}
-                    <Switch>
-                        <Route exact path="/about" component={Home} />
-                        <Route exact path="/" component={About} />
-                        <Route exact path="/projects" component={Project} />
-                        <Route exact path="/blog" component={Blog} />
-                        <Route exact path="/contact" component={Contact} />
+  return (
+    <Router>
+      {/* <NavBar /> */}
+      <div class="row">
+        <div class="col-md-3 left-menu">
+          <SideBar />
+        </div>
 
-                        <Container> 
-                        <Route path="/movies/list" exact component={MoviesList} />
-                        <Route path="/movies/create" exact component={MoviesInsert} />
-                        <Route
-                            path="/movies/update/:id"
-                            exact
-                            component={MoviesUpdate}
-                        />
-                        </Container>
-                    </Switch>
-                </div>
+        <div class="col-md-9 col-sm-11">
+          <div class="content">
+            <div class="nav-header">
+              <Header />
             </div>
-        </Router>
-    )
+            <Switch>
+              <Route exact path="/about" component={Home} />
+              <Route exact path="/" component={About} />
+              <Route exact path="/projects" component={Project} />
+              <Route exact path="/blog" component={Blog} />
+              <Route exact path="/contact" component={Contact} />
+
+              <Container>
+                <Route path="/movies/list" exact component={MoviesList} />
+                <Route path="/movies/create" exact component={MoviesInsert} />
+                <Route
+                  path="/movies/update/:id"
+                  exact
+                  component={MoviesUpdate}
+                />
+              </Container>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,52 +5,59 @@ class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                <div class="common-card">
-                    <div class="heding" uk-scrollspy="cls: uk-animation-slide-left;">Hello, I am Mehedi Hasan</div>
-                    <div class="aboutme" uk-scrollspy="cls: uk-animation-slide-right;">
-                     <h2>A Full Stack Laravel and ReactJS Web/Software Developer</h2> 
-                     <p>I have 2 years 5 months working experience in different software company. Javascript, nodeJS Mongodb, express, featherjs, React, react native, electronjs are my top key skills and gathered experince through invoving myself in development of many desktop application , Web application.</p>
-                     <p>Started my career as a Software Engineer and I got the opportunity to work in different companies with different teams and technologies. From there, I have learned a lot about Research and Development which is a key factor in order to solve newer problems.</p>
-                     <p>Having a Computer Science background, I look forward to learning and research more and more about new technology and ready to face all kinds of challenges with a view to establishing myself as someone with excellent creativity, professional skills and leadership abilities which I would love to implement in real life for making this world a much better place.</p>
-                    </div>
-                </div>
-                <div class="common-card" uk-grid uk-scrollspy="cls: uk-animation-slide-bottom; target: .uk-card; delay: 300; repeat: true">
-                    <div class="about">
-                        <span class="lftBorder"></span>
-                        <div class="aboutCont uk-card uk-card-default uk-card-body">
-                            <div class="aboutHdr"><span>December, 2018 - Present</span></div>
-                            <div class="aboutItem">
-                                <h2>Software engineer</h2>
-                                <h3>Mars Solution Ltd</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
-                            </div>
-                        </div>
-                        <div class="aboutCont uk-card uk-card-default uk-card-body">
-                            <div class="aboutHdr"><span>December, 2018 - Present</span></div>
-                            <div class="aboutItem">
-                                <h2>Software engineer</h2>
-                                <h3>Mars Solution Ltd</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
-                            </div>
-                        </div>
-                        <div class="aboutCont uk-card uk-card-default uk-card-body">
-                            <div class="aboutHdr"><span>December, 2018 - Present</span></div>
-                            <div class="aboutItem">
-                                <h2>Software engineer</h2>
-                                <h3>Mars Solution Ltd</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
-                            </div>
-                        </div>
-                        <div class="aboutCont uk-card uk-card-default uk-card-body">
-                            <div class="aboutHdr"><span>December, 2018 - Present</span></div>
-                            <div class="aboutItem">
-                                <h2>Software engineer</h2>
-                                <h3>Mars Solution Ltd</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
+                <div class="col-md-9 col-sm-12">
+                    <div class="common-card">
+                        <div class="aboutme" uk-scrollspy="cls: uk-animation-slide-right;">
+
+                            <div class="about-info">
+
+                            <h5>A Full Stack Laravel Vue js and React JS Software Developer</h5> 
+                                <p>I have 2 years 5 months working experience in different software company. Javascript, nodeJS Mongodb, express, featherjs, React, react native, electronjs are my top key skills and gathered experince through invoving myself in development of many desktop application , Web application.</p>
+                                <p>Started my career as a Software Engineer and I got the opportunity to work in different companies with different teams and technologies. From there, I have learned a lot about Research and Development which is a key factor in order to solve newer problems.</p>
+                                <p>Having a Computer Science background, I look forward to learning and research more and more about new technology and ready to face all kinds of challenges with a view to establishing myself as someone with excellent creativity, professional skills and leadership abilities which I would love to implement in real life for making this world a much better place.</p>
                             </div>
                         </div>
                     </div>
+
+                    <div class="common-card" uk-grid uk-scrollspy="cls: uk-animation-slide-left; target: .uk-card; delay: 300; repeat: true">
+                        <div class="about">
+                            <span class="lftBorder"></span>
+                            <div class="aboutCont uk-card uk-card-default uk-card-body">
+                                <div class="aboutHdr"><span>December, 2018 - Present</span></div>
+                                <div class="aboutItem">
+                                    <h4>Software engineer</h4>
+                                    <h5>Mars Solution Ltd</h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
+                                </div>
+                            </div>
+                            <div class="aboutCont uk-card uk-card-default uk-card-body">
+                                <div class="aboutHdr"><span>December, 2018 - Present</span></div>
+                                <div class="aboutItem">
+                                    <h4>Software engineer</h4>
+                                    <h5>Mars Solution Ltd</h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
+                                </div>
+                            </div>
+                            <div class="aboutCont uk-card uk-card-default uk-card-body">
+                                <div class="aboutHdr"><span>December, 2018 - Present</span></div>
+                                <div class="aboutItem">
+                                    <h4>Software engineer</h4>
+                                    <h5>Mars Solution Ltd</h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
+                                </div>
+                            </div>
+                            <div class="aboutCont uk-card uk-card-default uk-card-body">
+                                <div class="aboutHdr"><span>December, 2018 - Present</span></div>
+                                <div class="aboutItem">
+                                    <h4>Software engineer</h4>
+                                    <h5>Mars Solution Ltd</h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati voluptates nostrum. Odio quis aliquid minima, voluptatibus voluptatum eveniet, nemo architecto adipisci eligendi repellat nobis nostrum. Officia beatae illo veritatis.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                
                 </React.Fragment>
         )
     }
