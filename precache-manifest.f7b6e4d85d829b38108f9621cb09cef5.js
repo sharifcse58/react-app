c762ef69c0e4102787841a583d0347d6195fd930
sharifcse58 <sharifcse57@gmail.com>
2022-02-20 18:59:28 +0000

    Deploying to gh-pages from @ sharifcse58/react-app@a80e5c1f6accedce739dcff4cbb47af2c97d1ac0 🚀

--- precache-manifest.f7b6e4d85d829b38108f9621cb09cef5.js
+++ precache-manifest.f7b6e4d85d829b38108f9621cb09cef5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c92f440d6a284c08301c76bbd08fa667",
+    "revision": "89df36c0d2caf7a2e34cdddb50078cc5",
     "url": "/react-app/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-app/static/css/2.e92c2c3e.chunk.css"
   },
   {
-    "revision": "1986a7666ed6a33fb804",
-    "url": "/react-app/static/css/main.117ebac2.chunk.css"
+    "revision": "6e9731ee4271f634ce0e",
+    "url": "/react-app/static/css/main.97027e7b.chunk.css"
   },
   {
     "revision": "9efa2648d756ddfd7c8c",
     "url": "/react-app/static/js/2.22eae27d.chunk.js"
   },
   {
-    "revision": "1986a7666ed6a33fb804",
-    "url": "/react-app/static/js/main.752e2d0b.chunk.js"
+    "revision": "6e9731ee4271f634ce0e",
+    "url": "/react-app/static/js/main.42327ce6.chunk.js"
   },
   {
     "revision": "9ed022644c6f38b380ac",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.6b81d790db66a8cacb778988f8b65b9c.js"
+  "/react-app/precache-manifest.f7b6e4d85d829b38108f9621cb09cef5.js"
 );
 
 self.addEventListener('message', (event) => {
